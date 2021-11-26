f2689307ff7db171a94d1d5acaf08e7c97d525e4
Bernie Reilly <reilly_bernie@mac.com>
2021-11-26 16:12:45 -0500

    lots of updated

--- make_labels_report_lab.py
+++ make_labels_report_lab.py
@@ -7,52 +7,107 @@ list the room(s)???
 tracked or not
 '''
 import os
+from reportlab.lib.rl_accel import instanceStringWidthTTF
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
+from reportlab.lib import colors
+from reportlab.pdfbase.pdfmetrics import stringWidth
 
 import pandas as pd
+import math
+import re
 
-def colorsRGB(canvas, list):
-    from reportlab.lib import colors
-    # from reportlab.lib.units import inch
-    num_colums = 3
-    y= 650
-    x = 0; dy=inch*2; dx=inch*5.5/num_colums; w=h=dy/2; rdx=(dx-w)/2
-    rdy=h/5.0; texty=h+2*rdy
-    canvas.setFont("Helvetica",10)
+# inputs
+# big_h = 20*inch  #?
+# big_w = 4*inch
+box_w = 2*inch
+box_h = inch
+start_point = 11*inch  # top of page
+box_line=6
+fudge=32
+panels = ['subpanel', 'main']
+the_font = "Courier"
+the_font = "Helvetica"
 
-    canvas.setLineWidth(6)
-    # make rounded corners
-    canvas.setLineJoin(1)
-    canvas.setStrokeColor(colors.blue)
-    # set line details in the rect call or can set them before
-    canvas.rect(x+rdx, y+rdy, w, h, fill=0, strokeColor=colors.blue, strokeWidth=6)
-    # canvas.setFillColor()
-    # canvas.drawCentredString(x+dx/2, y+texty, name)
-    textobject = canvas.beginText(x+rdx+5, y+rdy+h-16)
-    textobject.setFont("Helvetica-Oblique", 8)
-    for circuit in list:    
-        textobject.textLine(circuit.strip().capitalize())
-    canvas.drawText(textobject)
-    x = x+dx
 
-if __name__ == "__main__":
-    df = pd.read_csv("test.csv")
-    c = canvas.Canvas("hello.pdf")
+dtypes = {
+    'room': 'string',
+    # 'amp': int,
+    }
+df = pd.read_csv(
+    "breaker panel labels.csv",
+ dtype=dtypes,
+#   keep_default_na=False
+  )
+for panel in panels:
+        
+    c = canvas.Canvas(panel + "_labels.pdf")
     # move the origin up and to the left
-    c.translate(inch,inch)
+    # c.translate(inch,inch)
     # textobject = c.beginText(0, 650)
-    # textobject.setFont("Helvetica-Oblique", 14)
+    # textobject.setFont(the_font, 14)
     # c.rect(0,650,200,30)
-    circuits = []
-    for circuit in df.iloc[1].circuits.split(';'):
-        circuits.append(circuit.strip().capitalize())
-        # textobject.textLine(circuit.strip().capitalize())
-    
-    # c.drawText(textobject)
-    # good example of putting shit in space
-    colorsRGB(c, circuits)
+    for index, row in df.iterrows():
+        if row.panel != panel:
+            continue
+        try:
+            if math.isnan(row.circuits):
+                row.circuits = ""
+        except TypeError:
+            pass
+        # get row on the physical paper sheet
+        paper_row = math.ceil(row.number/2)
+        paper_col = (row.number - 1) % 2
+        # parse text for circuits
+        circuits = []
+        for circuit in re.split('[;|,|.]',row.circuits):
+            circuits.append(circuit.strip().capitalize())
+        # good example of putting shit in space
+        y = start_point - paper_row*inch
+        x = paper_col * box_w
 
+        # c.setFont("Helvetica",14)
+        c.setLineWidth(box_line)
+        # make rounded corners
+        c.setLineJoin(1)
+        if row.floor == "basement":
+            c.setStrokeColor(colors.blue)
+        elif row.floor == "1st":
+            c.setStrokeColor(colors.green)
+        elif row.floor == "2nd":
+            c.setStrokeColor(colors.red)
+        else:
+            c.setStrokeColor(colors.gray)
+        # set line details in the rect call or can set them before
+        c.rect(x+fudge, y, box_w-fudge, box_h, fill=0)
+        # c.setFillColor()
+        # c.drawCentredString(x+dx/2, y+texty, name)
+        textobject = c.beginText(x+box_line+fudge, y+box_h-box_line*2)
+        textobject.setFont(the_font, 10)
+        try:
+            room_name = row.room.strip().capitalize()
+        except AttributeError:
+            room_name = ''
+        textWidth = stringWidth(room_name, the_font, 10)
+        cur_offset = (box_w-fudge)/2 - textWidth / 2 - box_line
+        textobject.moveCursor(cur_offset,0)
+        textobject.setFont(the_font+"-Bold", 10)
+        textobject.textLine(room_name)
+        textobject.moveCursor(-cur_offset,0)
+        textobject.setFont(the_font, 10)
+
+        for circuit in circuits:    
+            textobject.textLine(circuit.strip().capitalize())
+        c.drawText(textobject)
+        # make circuit number label
+        circ_num_obj = c.beginText(x+box_line, y+box_h/2-box_line+5)
+        circ_num_obj.setFont(the_font, 19)
+        circ_num_obj.textLine(str(row.number))
+        if not math.isnan(row.amp):
+            circ_num_obj.setFont(the_font, 6)
+            circ_num_obj.textLine(str(int(row.amp)))
+            circ_num_obj.textLine("Amps")
+
+        c.drawText(circ_num_obj)
     c.showPage()
     c.save()
-
