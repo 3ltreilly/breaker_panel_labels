d45ca7b4aeef98abf4ca38803abe3c977eac8a65
Bernie Reilly <reilly_bernie@mac.com>
2021-12-06 19:25:32 -0500

    added lots

--- make_labels_report_lab.py
+++ make_labels_report_lab.py
@@ -18,11 +18,9 @@ import math
 import re
 
 # inputs
-# big_h = 20*inch  #?
-# big_w = 4*inch
 box_w = 2*inch
 box_h = inch
-start_point = 11*inch  # top of page
+start_point = [0,11*inch ] # top of page
 box_line=6
 fudge=32
 panels = ['subpanel', 'main']
@@ -32,11 +30,15 @@ the_font = "Helvetica"
 
 dtypes = {
     'room': 'string',
-    # 'amp': int,
+    'vue_number': 'string',
+    # 'amp': 'int'
     }
 df = pd.read_csv(
     "breaker panel labels.csv",
- dtype=dtypes,
+    keep_default_na=False,
+    na_values='',
+    na_filter=False,
+    dtype=dtypes,
 #   keep_default_na=False
   )
 for panel in panels:
@@ -50,6 +52,12 @@ for panel in panels:
     for index, row in df.iterrows():
         if row.panel != panel:
             continue
+        # check for 20th breaker
+        if row.number >= 21:
+            start_point = [box_w*2,10*inch + 11*inch]
+        else:
+            start_point = [0,11*inch]
+
         try:
             if math.isnan(row.circuits):
                 row.circuits = ""
@@ -63,36 +71,40 @@ for panel in panels:
         for circuit in re.split('[;|,|.]',row.circuits):
             circuits.append(circuit.strip().capitalize())
         # good example of putting shit in space
-        y = start_point - paper_row*inch
-        x = paper_col * box_w
+        y = start_point[1] - paper_row*inch
+        x = start_point[0] + paper_col * box_w
 
         # c.setFont("Helvetica",14)
-        c.setLineWidth(box_line)
-        # make rounded corners
-        c.setLineJoin(1)
+
         if row.floor == "basement":
             c.setStrokeColor(colors.blue)
         elif row.floor == "1st":
             c.setStrokeColor(colors.green)
         elif row.floor == "2nd":
             c.setStrokeColor(colors.red)
+        elif row.floor == "outside":
+            c.setStrokeColor(colors.brown)
         else:
             c.setStrokeColor(colors.gray)
         # set line details in the rect call or can set them before
+        c.setLineWidth(box_line)
+        # make rounded corners
+        c.setLineJoin(1)
         c.rect(x+fudge, y, box_w-fudge, box_h, fill=0)
         # c.setFillColor()
         # c.drawCentredString(x+dx/2, y+texty, name)
         textobject = c.beginText(x+box_line+fudge, y+box_h-box_line*2)
         textobject.setFont(the_font, 10)
+        # add room name
         try:
             room_name = row.room.strip().capitalize()
         except AttributeError:
             room_name = ''
-        textWidth = stringWidth(room_name, the_font, 10)
-        cur_offset = (box_w-fudge)/2 - textWidth / 2 - box_line
-        textobject.moveCursor(cur_offset,0)
-        textobject.setFont(the_font+"-Bold", 10)
-        textobject.textLine(room_name)
+        textWidth = stringWidth(room_name, the_font, 10)  #get length of string
+        cur_offset = (box_w-fudge)/2 - textWidth / 2 - box_line  #fake a horizontal center layout
+        textobject.moveCursor(cur_offset,0)  
+        textobject.setFont(the_font+"-Bold", 10)  #make bold
+        textobject.textLine(room_name) # set name
         textobject.moveCursor(-cur_offset,0)
         textobject.setFont(the_font, 10)
 
@@ -103,10 +115,25 @@ for panel in panels:
         circ_num_obj = c.beginText(x+box_line, y+box_h/2-box_line+5)
         circ_num_obj.setFont(the_font, 19)
         circ_num_obj.textLine(str(row.number))
-        if not math.isnan(row.amp):
+        # add amp number
+        if row.amp:
             circ_num_obj.setFont(the_font, 6)
-            circ_num_obj.textLine(str(int(row.amp)))
+            circ_num_obj.textLine(row.amp)
             circ_num_obj.textLine("Amps")
+        # add Empria tracking number
+        if row.vue_number:
+            tweak = [-3,-1]
+            if len(row.vue_number) == 1:
+                c.drawString(x+box_w-10+tweak[0],y+10+tweak[1], row.vue_number)
+                c.setLineWidth(2)
+                c.setStrokeColor(colors.darkblue)
+                c.circle(x+box_w-8+tweak[0],y+13+tweak[1], 0.1*inch, fill=0)
+            else:
+                c.drawString(x+box_w-15+tweak[0],y+10+tweak[1], row.vue_number)
+                c.setLineWidth(2)
+                c.setStrokeColor(colors.darkblue)
+                c.circle(x+box_w-10+tweak[0],y+13+tweak[1], 0.13*inch, fill=0)                
+
 
         c.drawText(circ_num_obj)
     c.showPage()
