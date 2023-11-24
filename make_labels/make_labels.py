d0c0b3c6c88511b1ccaa8d8634023096c75d50f9
3ltreilly <42299256+3ltreilly@users.noreply.github.com>
2023-11-24 07:28:16 -0500

    Docs test (#1)
    * First commit
    
    * More settings
    
    * Moved
    
    * Add plugins
    
    * I don't remember
    
    * Upgrade poetry dev group
    
    * Fix path
    
    * Various pylint fixes
    
    * First commit
    
    * Update file name
    
    * Make better file name, fix rename modual bug
    
    * First commit
    
    * Fix typo, add comment
    
    * better wording

--- make_labels/make_labels.py
+++ make_labels/make_labels.py
@@ -4,10 +4,10 @@ Returns:
     pdf: PDF of labels
 """
 import math
-import re
-import tkinter as tk
 from pathlib import Path
+import re
 from tkinter import filedialog
+import tkinter as tk
 
 import click
 import pandas as pd
@@ -16,7 +16,9 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.pdfgen import canvas
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Table
+from reportlab.platypus import TableStyle
 
 
 def parse_circuits(row):
@@ -43,7 +45,7 @@ def parse_circuits(row):
     return circuits
 
 
-def set_floor_color(c, row):
+def set_floor_color(the_canvas, row):
     """Set box color based on floor
 
     Args:
@@ -51,30 +53,35 @@ def set_floor_color(c, row):
         row (array): row from csv file
     """
     if row.floor == "basement":
-        c.setStrokeColor(colors.blue)
+        the_canvas.setStrokeColor(colors.blue)
     elif row.floor == "1st":
-        c.setStrokeColor(colors.green)
+        the_canvas.setStrokeColor(colors.green)
     elif row.floor == "2nd":
-        c.setStrokeColor(colors.red)
+        the_canvas.setStrokeColor(colors.red)
     elif row.floor == "outside":
-        c.setStrokeColor(colors.brown)
+        the_canvas.setStrokeColor(colors.brown)
     else:
-        c.setStrokeColor(colors.gray)
+        the_canvas.setStrokeColor(colors.gray)
 
 
 # inputs
-box_w = 2 * inch
-box_h = inch
+BOX_W = 2 * inch
+BOX_H = inch
 start_point = [0, 11 * inch]  # top of page
-box_line = 6
-fudge = 32
-the_font = "Courier"
-the_font = "Helvetica"
+BOX_LINE = 6
+FUDGE = 32
+# THE_FONT = "Courier"
+THE_FONT = "Helvetica"
 
 
 @click.command()
 @click.argument("csv_file", type=click.Path(exists=True), required=False)
 def cli(csv_file):
+    """Make color coded breaker panel label
+
+    Args:
+        csv_file (str): file with panel info.
+    """
     print("current directory is")
     print(Path.cwd())
 
@@ -106,14 +113,16 @@ def cli(csv_file):
     panels = df.panel.unique()
 
     for panel in panels:
-
-        c = canvas.Canvas(csv_file.with_name(panel + "_labels.pdf").as_posix())
-        for index, row in df.iterrows():
+        # make file names bases on input file and panel column
+        label_file = f"{csv_file.stem}_{panel}_labels.pdf"
+        table_file = f"{csv_file.stem}_{panel}_table.pdf"
+        can = canvas.Canvas(label_file)
+        for _index, row in df.iterrows():
             if row.panel != panel:
                 continue
             # check for 20th breaker
             if row.number >= 21:
-                start_point = [box_w * 2, 10 * inch + 11 * inch]
+                start_point = [BOX_W * 2, 10 * inch + 11 * inch]
             else:
                 start_point = [0, 11 * inch]
 
@@ -129,74 +138,74 @@ def cli(csv_file):
             circuits = parse_circuits(row)
             # good example of putting shit in space
             y = start_point[1] - paper_row * inch
-            x = start_point[0] + paper_col * box_w
+            x = start_point[0] + paper_col * BOX_W
 
-            set_floor_color(c, row)
+            set_floor_color(can, row)
             # set line details in the rect call or can set them before
-            c.setLineWidth(box_line)
+            can.setLineWidth(BOX_LINE)
             # make rounded corners
-            c.setLineJoin(1)
-            c.rect(x + fudge, y, box_w - fudge, box_h, fill=0)
+            can.setLineJoin(1)
+            can.rect(x + FUDGE, y, BOX_W - FUDGE, BOX_H, fill=0)
 
-            textobject = c.beginText(x + box_line + fudge, y + box_h - box_line * 2)
-            textobject.setFont(the_font, 10)
+            textobject = can.beginText(x + BOX_LINE + FUDGE, y + BOX_H - BOX_LINE * 2)
+            textobject.setFont(THE_FONT, 10)
             # add room name
             try:
                 room_name = row.room.strip().capitalize()
             except AttributeError:
                 room_name = ""
-            textWidth = stringWidth(room_name, the_font, 10)  # get length of string
+            text_width = stringWidth(room_name, THE_FONT, 10)  # get length of string
             cur_offset = (
-                (box_w - fudge) / 2 - textWidth / 2 - box_line
+                (BOX_W - FUDGE) / 2 - text_width / 2 - BOX_LINE
             )  # fake a horizontal center layout
             textobject.moveCursor(cur_offset, 0)
-            textobject.setFont(the_font + "-Bold", 10)  # make bold
+            textobject.setFont(THE_FONT + "-Bold", 10)  # make bold
             textobject.textLine(room_name)  # set name
             textobject.moveCursor(-cur_offset, 0)
-            textobject.setFont(the_font, 10)
+            textobject.setFont(THE_FONT, 10)
 
             for circuit in circuits:
                 textobject.textLine(circuit.strip().capitalize())
-            c.drawText(textobject)
+            can.drawText(textobject)
             # make circuit number label
-            circ_num_obj = c.beginText(x + box_line, y + box_h / 2 - box_line + 5)
-            circ_num_obj.setFont(the_font, 19)
+            circ_num_obj = can.beginText(x + BOX_LINE, y + BOX_H / 2 - BOX_LINE + 5)
+            circ_num_obj.setFont(THE_FONT, 19)
             circ_num_obj.textLine(str(row.number))
             # add amp number
             if row.amp:
-                circ_num_obj.setFont(the_font, 6)
+                circ_num_obj.setFont(THE_FONT, 6)
                 circ_num_obj.textLine(row.amp)
                 circ_num_obj.textLine("Amps")
             # add Emporia tracking number
             if row.vue_number:
                 tweak = [-3, -1]
                 if len(row.vue_number) == 1:
-                    c.drawString(
-                        x + box_w - 10 + tweak[0], y + 10 + tweak[1], row.vue_number
+                    can.drawString(
+                        x + BOX_W - 10 + tweak[0], y + 10 + tweak[1], row.vue_number
                     )
-                    c.setLineWidth(2)
-                    c.setStrokeColor(colors.darkblue)
-                    c.circle(
-                        x + box_w - 8 + tweak[0], y + 13 + tweak[1], 0.1 * inch, fill=0
+                    can.setLineWidth(2)
+                    can.setStrokeColor(colors.darkblue)
+                    can.circle(
+                        x + BOX_W - 8 + tweak[0], y + 13 + tweak[1], 0.1 * inch, fill=0
                     )
                 else:
-                    c.drawString(
-                        x + box_w - 15 + tweak[0], y + 10 + tweak[1], row.vue_number
+                    can.drawString(
+                        x + BOX_W - 15 + tweak[0], y + 10 + tweak[1], row.vue_number
                     )
-                    c.setLineWidth(2)
-                    c.setStrokeColor(colors.darkblue)
-                    c.circle(
-                        x + box_w - 10 + tweak[0],
+                    can.setLineWidth(2)
+                    can.setStrokeColor(colors.darkblue)
+                    can.circle(
+                        x + BOX_W - 10 + tweak[0],
                         y + 13 + tweak[1],
                         0.13 * inch,
                         fill=0,
                     )
-            c.drawText(circ_num_obj)
-        c.showPage()
-        c.save()
+            can.drawText(circ_num_obj)
+        can.showPage()
+        can.save()
 
         doc = SimpleDocTemplate(
-            csv_file.with_name(panel + "_circuit_table.pdf").as_posix(),
+            csv_file.with_name(table_file).as_posix(),
             pagesize=letter,
             topMargin=inch / 2,
         )
